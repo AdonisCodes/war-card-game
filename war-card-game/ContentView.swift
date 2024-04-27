964869dc860409d5639ef0ee79dc551dadc76f30
Simon Ferns <business@simonferns.com>
2024-04-27 09:45:49 +0200

    Implemented Fully Functional game + Haptic Feedback!

--- war-card-game/ContentView.swift
+++ war-card-game/ContentView.swift
@@ -1,6 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard = "card6";
+    @State var cpuCard = "card9";
+    @State var playerScore: Int = 0;
+    @State var cpuScore: Int = 0;
+    
+    func toCardString(num: Int) -> String {
+        return "card"+String(num)
+    }
+    
     var body: some View {
         ZStack {
             Image("background").ignoresSafeArea()
@@ -15,17 +24,54 @@ struct ContentView: View {
                 
                 HStack {
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 
                 Spacer()
+                if playerScore > cpuScore {
+                    Image(systemName: "hand.thumbsup.fill")
+                } else if playerScore < cpuScore {
+                    Image(systemName: "hand.thumbsdown.fill")
+                } else {
+                    Image(systemName: "scribble")
+                }
                 
-                Button(action: {print("HI")}, label: {
+                Button(action: {
+                    let newPlayerCardInt = Int.random(in: 2..<15)
+                    let newCPUCardInt = Int.random(in: 2..<15)
+                    playerCard = toCardString(num: newPlayerCardInt)
+                    cpuCard = toCardString(num: newCPUCardInt)
+                    if newPlayerCardInt > newCPUCardInt {
+                        let impactMed = UIImpactFeedbackGenerator(style: .rigid)
+                        impactMed.impactOccurred()
+                        
+                        playerScore += 1
+                    } else {
+                        cpuScore += 1
+                    }
+                }, label: {
                     Image("dealbutton")
                 })
+                
+                // Cheating
+//                Button(action: {playerScore += 100}){
+//                    Text("Cheat Player")
+//                }
+//                Button(action: {cpuScore += 100}){
+//                    Text("Cheat Bot")
+//                }
+                
+//                Button("Click Me!"){
+//                    print("You clicked me!")
+//                }
+//                
+//                Button(action: {print("Click Me!")}){
+//                    Image(systemName: "app.fill")
+//                    Text("HI")
+//                }
                 
                 Spacer()
                 
@@ -36,7 +82,7 @@ struct ContentView: View {
                             .font(.subheadline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
@@ -46,7 +92,7 @@ struct ContentView: View {
                             .font(.subheadline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
